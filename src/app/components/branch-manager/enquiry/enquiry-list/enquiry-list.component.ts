badcb7118d78e378a9b921648af3ff6c2dc07d65
Senthilkumar Selvaraj <senthilkumar.selvaraj@adcltech.com>
2021-05-11 12:56:38 +0530

    enquiry list

--- src/app/components/branch-manager/enquiry/enquiry-list/enquiry-list.component.ts
+++ src/app/components/branch-manager/enquiry/enquiry-list/enquiry-list.component.ts
@@ -1,6 +1,8 @@
 /** Angular Imports */
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 /** Custom Forms */
 import { FormGroup, FormControl, Validators } from '@angular/forms';
@@ -9,9 +11,28 @@ import {  CrudService } from '../../../../services/crud.service';
 import { appModels } from '../../../../services/utils/enum.util';
 import { ToastrService } from 'ngx-toastr';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 import { untilDestroyed,UntilDestroy } from '@ngneat/until-destroy';
 @UntilDestroy({ checkProperties: true })
+
 
 
 /** Enquiry List Component */
@@ -21,6 +42,14 @@ import { untilDestroyed,UntilDestroy } from '@ngneat/until-destroy';
   styleUrls: ['./enquiry-list.component.scss']
 })
 export class EnquiryListComponent implements OnInit {
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','Action'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
   
   submitted: Boolean = false;
   showGenerateModel:Boolean = false;
@@ -40,6 +69,7 @@ export class EnquiryListComponent implements OnInit {
 
   ngOnInit(): void {
     this.getEnrollData();
+    this.saveEnquiry();
   }
 
   ngOnDestroy() { }
