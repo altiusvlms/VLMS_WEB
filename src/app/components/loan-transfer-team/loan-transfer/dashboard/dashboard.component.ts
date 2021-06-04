856e862783db4486185a798ff9505eb2cd9dfdc5
sathish98941 <satishkumar.gopal@adcltech.com>
2021-06-04 15:10:19 +0530

    logincompleted

--- src/app/components/loan-transfer-team/loan-transfer/dashboard/dashboard.component.ts
+++ src/app/components/loan-transfer-team/loan-transfer/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'mifosx-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  customerOnline(){
+    this.router.navigate(['loan-transfer-team/customer-online'])
+  }
+  loanAmountAdded(){
+    this.router.navigate(['loan-transfer-team/loan-amount-added'])
   }
 
 }
