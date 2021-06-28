594ab7723ed450c2fe366b865d00b3cd664d841a
sathish98941 <satishkumar.gopal@adcltech.com>
2021-06-28 18:55:52 +0530

    loantransferbackup

--- src/app/components/login/login/login.component.ts
+++ src/app/components/login/login/login.component.ts
@@ -47,6 +47,8 @@ export class LoginComponent implements OnInit {
           this.router.navigate(["/cashier/dashboard"]);
         }else if (response.roles[0].name === "LoanTransfer") {
           this.router.navigate(["/loan-transfer-team/dashboard"]);
+        }else if (response.roles[0].name === "super user") {
+          this.router.navigate(["/loan-transfer-team/dashboard"]);
         }
       
           localStorage.setItem('mobile_number', response.username);
